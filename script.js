0798ad63fef60bc0157090ce8a301dda25d56534
Joseph Wagner <wjoseph0@protonmail.com>
2021-08-27 18:29:26 -0500

    Reset functionality added

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function createCanvas() {
+function createCanvas(sqrPerSide) {
   const container = document.getElementById("container");
-  for (let i = 1; i <= (16*16); i++) {
+  container.style.gridTemplateColumns = `repeat(${sqrPerSide}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${sqrPerSide}, 1fr)`;
+
+  for (let i = 1; i <= (sqrPerSide*sqrPerSide); i++) {
     let cell = document.createElement('div');
     container.appendChild(cell);
   }
@@ -13,8 +16,27 @@ function draw() {
       div.style.backgroundColor = "black";
     });
   });
-
 }
 
-createCanvas();
+function resetCanvas() {
+  let resetButton = document.querySelector("#resetButton");
+  resetButton.addEventListener("click", function() {
+    
+    let cells = document.querySelectorAll('#container div');
+    cells.forEach(function(div) {
+      div.remove();
+    });
+
+    let chosenSides = prompt("How many squares per side?  Example: 50  Max amount: 100");
+    while (!chosenSides || chosenSides < 1 || chosenSides > 100) {
+      chosenSides = prompt("How many squares per side?  Example: 50  Max amount: 100");
+    }
+    
+    createCanvas(chosenSides);
+    draw();
+  });
+}
+
+createCanvas(16);
 draw();
+resetCanvas();
